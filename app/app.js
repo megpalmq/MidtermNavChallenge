4fb261b37f932c48948e7207af1e1f3846b7364e
Megan Palmquist <megpalmq@iu.edu>
2024-10-14 10:27:13 -0400

    added hamburger nav into index and nav styles

--- app/app.js
+++ app/app.js
@@ -1,14 +1,39 @@
-import * as MODEL from "../model/model.js"
+import * as MODEL from "../model/model.js";
 
+function navListeners() {
+  $("nav .hamburgerMenu").on("click", function (e) {
+    console.log($("nav .mobileClickables").css("transform"));
 
-function initListeners() {
-   $("nav a").on("click", (e) =>{
-        let btnID = e.currentTarget.id;
-        MODEL.changeRoute(btnID)
-   });
-
+    $("nav .mobileClickables").css("transition-duration", "0.2s");
+    if ($("nav .mobileClickables").css("transform")[16] == "0") {
+      $("nav .mobileClickables").css("transform", "translateY(100%) scaleY(100%)");
+    } else {
+      $("nav .mobileClickables").css("transform", "translateY(100%) scaleY(0)");
+    }
+  });
 }
- 
+
+function initURLListener() {
+  $(window).on("hashchange", function (e) {
+    MODEL.changeRoute();
+    updateActiveLinkInNav();
+  });
+  MODEL.changeRoute();
+  updateActiveLinkInNav();
+  navListeners();
+}
+
+function updateActiveLinkInNav() {
+  $("nav a").each(function (e) {
+    $(this).removeClass("active");
+    if (
+      $(this).attr("href") == window.location.hash ||
+      ($(this).attr("href") == "#home" && window.location.hash == "")
+    )
+      $(this).addClass("active");
+  });
+}
+
 $(document).ready(function () {
-    initListeners();
+  initURLListener();
 });
